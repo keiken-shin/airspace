62df6babcec5fb3d6d789122b6ce9403318078bc
keiken-shin <anubhav.kumar27@gmail.com>
2021-02-09 15:33:27 +0530

    Optimization

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+/* eslint-disable global-require */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/index.html', './src/**/*.jsx', './src/**/*.js'],
+  css: ['./src/assets/styles/tailwind.css'],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
   plugins: [
     require('tailwindcss')('./src/tailwind.config.js'),
     require('autoprefixer'),
+    process.env.REACT_APP_ENV === 'production' ? [purgecss] : [],
   ],
 };
